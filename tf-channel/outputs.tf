b5aaad7eb2ed5bdb550d905a7013d08c8cd003fb
Randy Keener <rjkeener@gmail.com>
2022-01-09 18:33:08 -0500

    Update outputs.tf

--- tf-channel/outputs.tf
+++ tf-channel/outputs.tf
@@ -2,6 +2,6 @@ output "ubuntu_iteration" {
   value = data.hcp_packer_iteration.ubuntu
 }
 
-output "ubuntu_us_east_2" {
+output "ubuntu_us_east_1" {
   value = data.hcp_packer_image.ubuntu_us_east_1
 }
